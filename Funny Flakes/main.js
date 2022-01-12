b9428743bc7b066e3c7b9ac9ffeae0ed89f57e04
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-01-12 16:24:37 +0100

    gotovo

--- Funny Flakes/main.js
+++ Funny Flakes/main.js
@@ -1,8 +1,13 @@
 const burger = document.querySelector (".burger");
 const navMenu = document.querySelector (".nav-menu");
+const subBtn = document.querySelector ("#subBtn");
 
 burger.addEventListener ("click", () => {
     burger.classList.toggle("open");
     navMenu.classList.toggle("open");
 });
 
+
+function subscribed() {
+    alert("Thank you for subscribing <3")
+};
